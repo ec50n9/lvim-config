8fb060ccb4664917c0ee8e352bd777603b9c1fc0
ec50n9 <shelloworld@qq.com>
2024-03-31 09:54:34 +0800

    修复vue文件中没有代码提示的问题

--- config.lua
+++ config.lua
@@ -1,8 +1,3 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
-
 -- 自定义配置
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
 vim.opt.relativenumber = true
@@ -12,12 +7,14 @@ vim.opt.cursorline = true   -- 高亮当前行
 vim.opt.cursorcolumn = true -- 高亮当前列
 
 lvim.plugins = {
+  -- 主题
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
   },
+  -- dart 相关
   { "dart-lang/dart-vim-plugin" },
   { "thosakwe/vim-flutter" },
 }
@@ -30,3 +27,21 @@ lvim.lsp.buffer_mappings.normal_mode['gh'] = { vim.lsp.buf.hover, "Show document
 lvim.keys.normal_mode['<S-h>'] = ":bprevious<CR>"
 lvim.keys.normal_mode['<S-l>'] = ":bnext<CR>"
 lvim.keys.normal_mode['<S-x>'] = ":BufferKill<CR>"
+
+-- volar 中开启 tsserver
+require('lspconfig')['tsserver'].setup({
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/home/ec50n9/.local/share/pnpm/global/5/node_modules/@vue/typescript-plugin",
+        languages = { "javascript", "typescript", "vue" },
+      },
+    }
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "vue",
+  }
+})
